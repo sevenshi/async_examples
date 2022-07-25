608540a1d4593bbce8513b83d86819368f832dcb
seven <seven.shi88@gmail.com>
2022-07-25 11:26:05 +0800

    _02_03_timer

---
+++ _02_03_timer/src/lib.rs
@@ -0,0 +1,65 @@
+// ANCHOR: imports
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll, Waker},
+    thread,
+    time::Duration,
+};
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+struct SharedState{
+    /// Whether or not the sleep time has elapsed
+    completed: bool,
+
+    /// The waker for the task that `TimerFuture` is running on.
+    /// The thread can use this after setting `completed = true` to tell
+    /// `TimerFuture`'s task to wake up, see that `completed = true`, and
+    /// move forward.
+    waker: Option<Waker>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl TimerFuture {
+    pub fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState{
+            completed: false,
+            waker: None,
+        }));
+        // Spawn the new thread
+        let thread_shared_state = shared_state.clone();
+        thread::spawn(move || {
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let  Some(waker) = shared_state.waker.take()  {
+                waker.wake()
+            }
+        });
+        TimerFuture { shared_state }
+    }
+}
+
+
+#[test]
+fn block_on_timer(){
+    futures::executor::block_on(async {
+        TimerFuture::new(Duration::from_secs(1)).await
+    })
+}
